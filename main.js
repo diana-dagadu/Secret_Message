9a69546b0c2b0e6cf424167f41f68db288000e16
diana-dagadu <diana.dagadu@amalitech.org>
2021-07-20 18:26:18 +0000

    created a simple JS test file

---
+++ main.js
@@ -0,0 +1,25 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+// console.log(secretMessage)
+
+// console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program');
+// console.log(secretMessage.length);
+// console.log(secretMessage)
+
+secretMessage[7] = 'right';
+// console.log(secretMessage)
+
+secretMessage.shift()
+// console.log(secretMessage)
+
+secretMessage.unshift('Programming')
+// console.log(secretMessage)
+
+secretMessage.splice(6, 5, 'know')
+// console.log(secretMessage)
+
+console.log(secretMessage.join(' '))
+
